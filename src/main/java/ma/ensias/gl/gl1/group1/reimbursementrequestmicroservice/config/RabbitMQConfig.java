59fe86ee4664c4a20ede59512d634b8c39ec07bb
mehdicharife <mehdicharife@yahoo.com>
2024-01-08 21:05:02 +0100

    rabbitmq setup

---
+++ src/main/java/ma/ensias/gl/gl1/group1/reimbursementrequestmicroservice/config/RabbitMQConfig.java
@@ -0,0 +1,37 @@
+package ma.ensias.gl.gl1.group1.reimbursementrequestmicroservice.config;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+    private String reimbursementRequestApprovedExchangeName;
+    
+    @Bean
+    public FanoutExchange reimbursementRequestApprovedExchange() {
+        return new FanoutExchange(reimbursementRequestApprovedExchangeName);
+    }
+
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
